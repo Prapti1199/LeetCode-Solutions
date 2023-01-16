36794baa94cbf6828263003b8bd60b6a6b041ed9
Prapti Pradip Shah <54852097+Prapti1199@users.noreply.github.com>
2023-01-17 00:07:33 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.py
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.py
@@ -1,32 +1,66 @@
 #User function Template for python3
-
+from collections import deque
 
 class Solution:
     
-    def dfs(self, adj, V, start, visited):
-        visited[start] = 2
+    # def dfs(self, adj, V, start, visited):
+    #     visited[start] = 2
         
-        for s in adj[start]:
-            if visited[s] == 0:
-               if self.dfs(adj, V, s, visited):
-                    return True
+        
+    #     for s in adj[start]:
+    #         if visited[s] == 0:
+    #           if self.dfs(adj, V, s, visited):
+    #                 return True
             
-            elif visited[s] == 2:
-                return True
+    #         elif visited[s] == 2:
+    #             return True
         
-        visited[start]= 1
-        return False
+    #     visited[start]= 1
+    #     print(visited)
+    #     return False
+    
+        
     
     #Function to detect cycle in a directed graph.
-    def isCyclic(self, V, adj):
-        # code here
-        visited = [0 for i in range(V)]
-        for i in range(V):
-            if visited[i] == 0:
-                if self.dfs(adj, V, i, visited):
-                    return True
+    # def isCyclic(self, V, adj):
+    #     # code here
+    #     visited = [0 for i in range(V)]
+    #     for i in range(V):
+    #         if visited[i] == 0:
+    #             if self.dfs(adj, V, i, visited):
+    #                 return True
                     
-        return False
+    #     return False
+    
+    def isCyclic(self, V, adj):
+        indegree = [0 for i in range(V)]
+        queue = deque()
+        
+        for i in range(V):
+            for j in adj[i]:
+                indegree[j] = indegree[j] + 1
+                
+        for i in range(V):
+            if indegree[i] == 0:
+                queue.append(i)
+                
+        count = 0
+        
+        while(queue):
+            node = queue.popleft()
+            count += 1
+            
+            for i in adj[node]:
+                indegree[i] -= 1
+                if indegree[i] == 0:
+                    queue.append(i)
+        
+        if count == V:
+            return False
+        else:
+            return True
+            
+                
 
 #{ 
  # Driver Code Starts
